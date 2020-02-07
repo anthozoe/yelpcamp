122a95747f9529d72743e770d292663be9a22704
anthozoe <zwitsaa@gmail.com>
2020-02-07 11:05:37 +0200

    i add pass

--- app.js
+++ app.js
@@ -20,9 +20,10 @@ var seedDB = require("./seeds");
 //seedDB();
 
 //------------------APP CONFIG----------------------
-mongoose.connect("mongodb://Zoe:<password>@yelpcamp-shard-00-00-edzg2.mongodb.net:27017,yelpcamp-shard-00-01-edzg2.mongodb.net:27017,yelpcamp-shard-00-02-edzg2.mongodb.net:27017/test?ssl=true&replicaSet=yelpcamp-shard-0&authSource=admin&retryWrites=true&w=majority", 
+//mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://Zoe:123@yelpcamp-shard-00-00-edzg2.mongodb.net:27017,yelpcamp-shard-00-01-edzg2.mongodb.net:27017,yelpcamp-shard-00-02-edzg2.mongodb.net:27017/test?ssl=true&replicaSet=yelpcamp-shard-0&authSource=admin&retryWrites=true&w=majority", 
 { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
-mongoose.Promise = global.Promise;
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
