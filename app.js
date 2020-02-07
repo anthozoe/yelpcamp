7391c1fb60f829cc3910f26a5e2d6047fb4042f6
anthozoe <zwitsaa@gmail.com>
2020-02-07 11:00:27 +0200

    add mongoose.promise

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var seedDB = require("./seeds");
 //------------------APP CONFIG----------------------
 mongoose.connect("mongodb://Zoe:<password>@yelpcamp-shard-00-00-edzg2.mongodb.net:27017,yelpcamp-shard-00-01-edzg2.mongodb.net:27017,yelpcamp-shard-00-02-edzg2.mongodb.net:27017/test?ssl=true&replicaSet=yelpcamp-shard-0&authSource=admin&retryWrites=true&w=majority", 
 { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
