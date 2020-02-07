02ca334bdb1e03889032689faa15e8cdefb33b5c
anthozoe <zwitsaa@gmail.com>
2020-02-07 11:51:16 +0200

    delete some chars uri

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var seedDB = require("./seeds");
 //seedDB();
 
 //------------------APP CONFIG----------------------
-mongoose.connect("mongodb://Zoe:123@yelpcamp-shard-00-00-edzg2.mongodb.net:27017,yelpcamp-shard-00-01-edzg2.mongodb.net:27017,yelpcamp-shard-00-02-edzg2.mongodb.net:27017/test?ssl=true&replicaSet=yelpcamp-shard-0&authSource=admin&retryWrites=true&w=majority", 
+mongoose.connect("mongodb://Zoe:123@yelpcamp-shard-00-02-edzg2.mongodb.net:27017/test?ssl=true&replicaSet=yelpcamp-shard-0&authSource=admin&retryWrites=true&w=majority", 
 { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 app.use(bodyParser.urlencoded({extended: true}));
